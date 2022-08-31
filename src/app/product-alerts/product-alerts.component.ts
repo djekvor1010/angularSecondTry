22cc9467664117f45fbb120ee9312aa253995049
djekvor1010 <ivan0679014@gmail.com>
2022-08-31 21:02:23 +0200

    5 files modified, 4 added

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -9,6 +9,7 @@ import { Product } from '../products';
 
 export class ProductAlertsComponent {
 
-  @Input() product!: Product;
+  @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
 
 }
